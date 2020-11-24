96654e84759443a9f08c571cd7e61366947325a8
Pedro Alvim <67213948+PedroAlvim@users.noreply.github.com>
2020-11-24 09:19:21 -0300

    Update color.cpp

--- Others/color.cpp
+++ Others/color.cpp
@@ -1,8 +1,8 @@
-char color(int color)
+std::string color(int color)
 {
     //init REF:https://stackoverflow.com/questions/4053837/colorizing-text-in-the-console-with-c
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, color);
     //end REF.
-    return '\0';
+    return "";
 }
